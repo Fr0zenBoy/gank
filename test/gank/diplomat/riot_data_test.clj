e005dc768abd9e79d8802989e5209d6cdf5d3e77
Fr0zenBoy <th129513@gmail.com>
2019-10-15 01:28:35 -0300

    create new tests

---
+++ test/gank/diplomat/riot_data_test.clj
@@ -0,0 +1,3 @@
+(ns gank.test.diplomat.riot-data-test
+  (:require [gank.diplomat.riot-data :as r.data]
+            [midje.sweet :refer :all]))
---
+++ test/gank/logic/commons_test.clj
@@ -0,0 +1,66 @@
+(ns gank.test.logic.commons-test
+  (:require [gank.logic.commons :as commons]
+            [midje.sweet :refer :all]))
+
+(def player-mastery [{:championId                   122
+                      :championPointsSinceLastLevel 333821
+                      :championPoints               355421
+                      :chestGranted                 true
+                      :championPointsUntilNextLevel 0
+                      :summonerId                   "pspyxm-GoK9qGIZHFVkg2NAxvWMitxhIHsVf_MgkkHJ6Ow"
+                      :lastPlayTime                 1570937680000
+                      :tokensEarned                 0
+                      :championLevel                7}
+                     {:championId                   98
+                      :championPointsSinceLastLevel 326638
+                      :championPoints               348238
+                      :chestGranted                 true
+                      :championPointsUntilNextLevel 0
+                      :summonerId                   "pspyxm-GoK9qGIZHFVkg2NAxvWMitxhIHsVf_MgkkHJ6Ow"
+                      :lastPlayTime                 1568424677000
+                      :tokensEarned                 0
+                      :championLevel                7}
+                     {:championId                   412
+                      :championPointsSinceLastLevel 270183
+                      :championPoints               291783
+                      :chestGranted                 true
+                      :championPointsUntilNextLevel 0
+                      :summonerId                   "pspyxm-GoK9qGIZHFVkg2NAxvWMitxhIHsVf_MgkkHJ6Ow"
+                      :lastPlayTime                 1569556500000
+                      :tokensEarned                 0
+                      :championLevel                7}])
+
+(fact "Return the first match of the predicate"
+      (commons/find-first even? [1 2 3 4]) => 2)
+
+(fact "Convert data into within a to string"
+      (commons/convert-int-str :championId player-mastery) => [{:championId                   "122"
+                                                                :championPointsSinceLastLevel 333821
+                                                                :championPoints               355421
+                                                                :chestGranted                 true
+                                                                :championPointsUntilNextLevel 0
+                                                                :summonerId                   "pspyxm-GoK9qGIZHFVkg2NAxvWMitxhIHsVf_MgkkHJ6Ow"
+                                                                :lastPlayTime                 1570937680000
+                                                                :tokensEarned                 0
+                                                                :championLevel                7}
+                                                               {:championId                   "98"
+                                                                :championPointsSinceLastLevel 326638
+                                                                :championPoints               348238
+                                                                :chestGranted                 true
+                                                                :championPointsUntilNextLevel 0
+                                                                :summonerId                   "pspyxm-GoK9qGIZHFVkg2NAxvWMitxhIHsVf_MgkkHJ6Ow"
+                                                                :lastPlayTime                 1568424677000
+                                                                :tokensEarned                 0
+                                                                :championLevel                7}
+                                                               {:championId                   "412"
+                                                                :championPointsSinceLastLevel 270183
+                                                                :championPoints               291783
+                                                                :chestGranted                 true
+                                                                :championPointsUntilNextLevel 0
+                                                                :summonerId                   "pspyxm-GoK9qGIZHFVkg2NAxvWMitxhIHsVf_MgkkHJ6Ow"
+                                                                :lastPlayTime                 1569556500000
+                                                                :tokensEarned                 0
+                                                                :championLevel                7}])
+
+(fact "Sum all values ​​of a map and return a single value"
+      (commons/sum-key-value :championPoints player-mastery) => 995442)
